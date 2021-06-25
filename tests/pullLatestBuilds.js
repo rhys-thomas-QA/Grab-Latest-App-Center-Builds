e0841856b187671651cef0fd3b92cd2d72fca09a
Rhys Thomas <53527841+rhys-thomas-QA@users.noreply.github.com>
2021-06-25 15:40:03 +1000

    Update pullLatestBuilds.js
    Update to not use app secrets, only need api key from app center

--- tests/pullLatestBuilds.js
+++ tests/pullLatestBuilds.js
@@ -1,44 +1,52 @@
 const request = require('axios')
 const dotenv = require("dotenv");
-const configs = require("../configs")
 const fs = require('fs');
 const extract = require('ipa-extract-info');
 const AppInfoParser = require('app-info-parser')
 
 dotenv.config();
 
-const getLatestBuilds = async () => {
+const getLatestBuilds = async (appName) => {
     let currentVersion;
     let appCenterResponse;
     let newVersion;
     let downloadLink;
 
-    const currentDevice = configs[`${process.env.CONFIG}`]
     const authHeaders = {
         "X-API-Token": `${process.env.MS_APP_CENTER_KEY}`
     }
+    const getAppsFromAppCenter = await request.get("https://api.appcenter.ms/v0.1/apps", {
+        headers: authHeaders
+    })
+    if(getAppsFromAppCenter.status == 401) {
+        console.log("You dont have the right access permissions with  app center API key - contact your admin to give you more permissions")
+    }
+    //Leave this console log in so user can see exactly what the app display name is called
+    // console.log(getAppsFromAppCenter.data);
 
-    if (currentDevice.platform == "iOS") {
-        if (fs.existsSync(currentDevice.app)) {
-            const fd = fs.openSync(currentDevice.app, 'r');
+    const indexOfAppInArray = getAppsFromAppCenter.data.map((element) => {return element.display_name}).indexOf(appName)
+    const appSecret = getAppsFromAppCenter.data[indexOfAppInArray].app_secret;
+    
+    if (getAppsFromAppCenter.data[indexOfAppInArray].os == "iOS") {
+        if (fs.existsSync(`apps/${appName}.ipa`)) {
+            const fd = fs.openSync(`apps/${appName}.ipa`, 'r');
             extract(fd, function (info, raw) {
-                console.log(raw); // the unparsed plist
                 currentVersion = raw[0].CFBundleShortVersionString
             });
         }
-        appCenterResponse = await request.get(`https://api.appcenter.ms/v0.1/sdk/apps/${process.env.IOS_APP_SECRET}/releases/latest`, {
+        appCenterResponse = await request.get(`https://api.appcenter.ms/v0.1/sdk/apps/${appSecret}/releases/latest`, {
             headers: authHeaders
         })
         newVersion = appCenterResponse.data.short_version
         downloadLink = appCenterResponse.data.download_url;
 
-    } else if (currentDevice.platform == "Android") {
-        if (fs.existsSync(currentDevice.app)) {
-            const parser = new AppInfoParser(currentDevice.app)
+    } else if (getAppsFromAppCenter.data[indexOfAppInArray].os == "Android") {
+        if (fs.existsSync(`apps/${appName}.apk`)) {
+            const parser = new AppInfoParser(`apps/${appName}.apk`)
             const parserResponse = await parser.parse();
             currentVersion = parserResponse.versionName
         }
-        appCenterResponse = await request.get(`https://api.appcenter.ms/v0.1/sdk/apps/${process.env.ANDROID_APP_SECRET}/releases/latest`, {
+        appCenterResponse = await request.get(`https://api.appcenter.ms/v0.1/sdk/apps/${appSecret}/releases/latest`, {
             headers: authHeaders
         });
         newVersion = appCenterResponse.data.short_version
@@ -63,9 +71,9 @@ const getLatestBuilds = async () => {
 
     if (currentVersion != newVersion) {
         console.log(`Current version on your machine is ${currentVersion}, newer version ${newVersion} is available - downloading latest version`)
-        if (currentDevice.platform == "iOS") {
+        if (getAppsFromAppCenter.data[indexOfAppInArray].os == "iOS") {
             await downloadFile("ipa", downloadLink);
-        } else if (currentDevice.platform == "Android") {
+        } else if (getAppsFromAppCenter.data[indexOfAppInArray].os == "Android") {
             await downloadFile("apk", downloadLink);
         }
     } else {
@@ -73,4 +81,4 @@ const getLatestBuilds = async () => {
     }
 }
 
-getLatestBuilds()
+getLatestBuilds("<NAME OF YOUR APP IN APP CENTER>")
